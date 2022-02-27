697c2e0f465bfd3842233194e3f06f8a013a1756
Myagmartseren <myagmartseren7@gmail.com>
2022-02-27 20:03:49 +0800

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-  ],
+  plugins: [],
 }
